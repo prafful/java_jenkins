bc804df4eeb2b7b6b8b323b8a2c45c28101fa1b1
Prafful Daga <prraful@gmail.com>
2019-12-23 12:22:25 +0530

    my second commit for jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ class HelloWorld{
 		System.out.println("Hello from Java Jenkins 4!!!!");
 		System.out.println("It was working!!!");
 		System.out.println("Chennai!!!");
+		for(int i=1; i<=10; i++){
+			System.out.println(i + " Chennai!!!");
+		}
 	}	
 
 }
