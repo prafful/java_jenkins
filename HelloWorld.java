bf2bcca082ac8317106de442478816cbe9a9e347
Prafful Daga <prraful@gmail.com>
2019-12-23 10:49:33 +0530

    my first commit for jenkins

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+
+	public static void main(String args[]){
+		System.out.println("Hello from Java Jenkins 4!!!!");
+		System.out.println("It was working!!!");
+		System.out.println("Chennai!!!");
+	}	
+
+}
