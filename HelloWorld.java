5709163b3a58deb37b554287775b9e4c7ae62f47
Prafful Daga <prraful@gmail.com>
2019-12-26 12:04:06 +0530

    fifth commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 class HelloWorld{
 
 	public static void main(String args[]){
-		System.out.println("Hello from Java Jenkins 4!!!!");
+		System.out.println("Hello from Java Jenkins!!!!");
 		System.out.println("It was working!!!");
 		System.out.println("Chennai!!!");
 		for(int i=1; i<=10; i++){
-			System.out.println(i + " Build Monitoring!!!");
+			System.out.println(i + " Build Monitoring Done!!!");
 		}
 	}	
 
